e85aab766b6c82a388863c61e3af6aec2b866306
Thiago Alves <153774707+thiagomkvl@users.noreply.github.com>
2026-02-03 11:51:50 -0300

    Update cnab_engine.py

--- modules/cnab_engine.py
+++ modules/cnab_engine.py
@@ -34,57 +34,61 @@ def gerar_cnab_pix(df_pagamentos):
     data_arq = now.strftime('%d%m%Y')
     hora_arq = now.strftime('%H%M%S')
     
-    # --- HEADER DE ARQUIVO (CORRIGIDO: 240 POSIÇÕES EXATAS) ---
-    # Correção: Ajustados os espaçamentos das posições 09-17 e 133-142
+    # --- HEADER DE ARQUIVO (240 POSIÇÕES) ---
     header_arq = (
-        f"13600000"                                 # 01-08: Controles
-        f"{'':<9}"                                  # 09-17: CNAB (9 espaços)
-        f"2"                                        # 18-18: Tipo Insc (CNPJ)
+        f"{'136':<3}"                               # 01-03: Banco
+        f"{'0000':<4}"                              # 04-07: Lote
+        f"{'0':<1}"                                 # 08-08: Registro
+        f"{'':<9}"                                  # 09-17: CNAB (9 brancos)
+        f"{'2':<1}"                                 # 18-18: Insc
         f"{DADOS_HOSPITAL['cnpj']:0>14}"            # 19-32: CNPJ
         f"{DADOS_HOSPITAL['convenio']:0>20}"        # 33-52: Convênio
         f"{DADOS_HOSPITAL['agencia']:0>5}"          # 53-57: Agência
-        f"0"                                        # 58-58: DV Ag
+        f"{'0':<1}"                                 # 58-58: DV Ag
         f"{DADOS_HOSPITAL['conta']:0>12}"           # 59-70: Conta
         f"{DADOS_HOSPITAL['dv_conta']:0>1}"         # 71-71: DV Conta
-        f"0"                                        # 72-72: DV Ag/Conta
-        f"{DADOS_HOSPITAL['nome']:<30}"             # 73-102: Nome Empresa
+        f"{'0':<1}"                                 # 72-72: DV Ag/Conta
+        f"{DADOS_HOSPITAL['nome']:<30}"             # 73-102: Nome Emp
         f"{'UNICRED':<30}"                          # 103-132: Nome Banco
-        f"{'':<10}"                                 # 133-142: CNAB (10 espaços)
-        f"1"                                        # 143-143: Código Remessa
-        f"{data_arq}"                               # 144-151: Data
-        f"{hora_arq}"                               # 152-157: Hora
-        f"{nsa:0>6}"                                # 158-163: Sequencial
-        f"083"                                      # 164-166: Versão
-        f"00000"                                    # 167-171: Densidade
-        f"{'':<69}"                                 # 172-240: Reservado
+        f"{'':<10}"                                 # 133-142: CNAB (10 brancos - ONDE OCORRIA O ERRO)
+        f"{'1':<1}"                                 # 143-143: Cod Remessa
+        f"{data_arq:<8}"                            # 144-151: Data
+        f"{hora_arq:<6}"                            # 152-157: Hora
+        f"{nsa:0>6}"                                # 158-163: NSA
+        f"{'083':<3}"                               # 164-166: Versão
+        f"{'00000':<5}"                             # 167-171: Densidade
+        f"{'':<69}"                                 # 172-240: Reservado (69 brancos)
         f"\r\n"
     )
 
     # --- HEADER DE LOTE (240 POSIÇÕES) ---
     header_lote = (
-        f"13600011"                                 # 01-08: Controles
-        f"C"                                        # 09-09: Operação
-        f"20"                                       # 10-11: Tipo Pagto
-        f"45"                                       # 12-13: Forma Pagto (Pix)
-        f"040"                                      # 14-16: Versão Layout
-        f" "                                        # 17-17: CNAB
-        f"2"                                        # 18-18: Tipo Insc
-        f"{DADOS_HOSPITAL['cnpj']:0>14}"            # 19-32: CNPJ
-        f"{DADOS_HOSPITAL['convenio']:0>20}"        # 33-52: Convênio
-        f"{DADOS_HOSPITAL['agencia']:0>5}"          # 53-57: Agência
-        f"0"                                        # 58-58: DV Ag
-        f"{DADOS_HOSPITAL['conta']:0>12}"           # 59-70: Conta
-        f"{DADOS_HOSPITAL['dv_conta']:0>1}"         # 71-71: DV Conta
-        f"0"                                        # 72-72: DV Ag/Conta
-        f"{DADOS_HOSPITAL['nome']:<30}"             # 73-102: Nome
-        f"{'':<40}"                                 # 103-142: Mensagem 1
+        f"{'136':<3}"                               # 01-03
+        f"{'0001':<4}"                              # 04-07
+        f"{'1':<1}"                                 # 08-08
+        f"{'C':<1}"                                 # 09-09
+        f"{'20':<2}"                                # 10-11
+        f"{'45':<2}"                                # 12-13
+        f"{'040':<3}"                               # 14-16
+        f"{'':<1}"                                  # 17-17
+        f"{'2':<1}"                                 # 18-18
+        f"{DADOS_HOSPITAL['cnpj']:0>14}"            # 19-32
+        f"{DADOS_HOSPITAL['convenio']:0>20}"        # 33-52
+        f"{DADOS_HOSPITAL['agencia']:0>5}"          # 53-57
+        f"{'0':<1}"                                 # 58-58
+        f"{DADOS_HOSPITAL['conta']:0>12}"           # 59-70
+        f"{DADOS_HOSPITAL['dv_conta']:0>1}"         # 71-71
+        f"{'0':<1}"                                 # 72-72
+        f"{DADOS_HOSPITAL['nome']:<30}"             # 73-102
+        f"{'':<40}"                                 # 103-142: Msg 1
         f"{'':<40}"                                 # 143-182: Endereço
-        f"{'':<8}"                                  # 183-190: Numero
-        f"{'':<15}"                                 # 191-205: Compl
-        f"{'':<20}"                                 # 206-225: Cidade
-        f"{'00000000'}"                             # 226-233: CEP
-        f"{'SC'}"                                   # 234-235: UF
-        f"{'':<5}"                                  # 236-240: CNAB
+        f"{'0':<5}"                                 # 183-187: Num
+        f"{'':<15}"                                 # 188-202: Compl
+        f"{'':<20}"                                 # 203-222: Cidade
+        f"{'00000':<5}"                             # 223-227: CEP
+        f"{'000':<3}"                               # 228-230: Sufixo CEP
+        f"{'SC':<2}"                                # 231-232: UF
+        f"{'':<8}"                                  # 233-240: CNAB
         f"\r\n"
     )
 
@@ -113,31 +117,33 @@ def gerar_cnab_pix(df_pagamentos):
 
         # --- SEGMENTO A (240 POSIÇÕES) ---
         seg_a = (
-            f"13600013"                             # 01-08
+            f"{'136':<3}"                           # 01-03
+            f"{'0001':<4}"                          # 04-07: Lote
+            f"{'3':<1}"                             # 08-08: Detalhe
             f"{seq_lote:0>5}"                       # 09-13
-            f"A"                                    # 14-14
-            f"000"                                  # 15-17: Inclusão
-            f"009"                                  # 18-20: Pix
-            f"136"                                  # 21-23: Banco
-            f"{'0':>5}"                             # 24-28: Agência (Zero p/ Pix)
-            f" "                                    # 29-29
-            f"{'0':>12}"                            # 30-41: Conta (Zero p/ Pix)
-            f" "                                    # 42-42
-            f" "                                    # 43-43
+            f"{'A':<1}"                             # 14-14
+            f"{'000':<3}"                           # 15-17
+            f"{'009':<3}"                           # 18-20: Pix
+            f"{'136':<3}"                           # 21-23: Banco Fav
+            f"{'0':>5}"                             # 24-28: Ag Fav
+            f"{' ':1}"                              # 29-29
+            f"{'0':>12}"                            # 30-41: Conta Fav
+            f"{' ':1}"                              # 42-42
+            f"{' ':1}"                              # 43-43
             f"{str(row['NOME_FAVORECIDO'])[:30]:<30}" # 44-73
-            f"{chave_pix_raw:<20}"                  # 74-93: Seu Numero (Chave)
-            f"{dt_str}"                             # 94-101
-            f"BRL"                                  # 102-104
+            f"{chave_pix_raw:<20}"                  # 74-93: Seu Numero
+            f"{dt_str:<8}"                          # 94-101
+            f"{'BRL':<3}"                           # 102-104
             f"{'0':>15}"                            # 105-119
-            f"{valor_str}"                          # 120-134
+            f"{valor_str:<15}"                      # 120-134
             f"{'':<20}"                             # 135-154
-            f"{dt_str}"                             # 155-162
-            f"{valor_str}"                          # 163-177
-            f"{'':<40}"                             # 178-217: Info
-            f"00"                                   # 218-219
+            f"{dt_str:<8}"                          # 155-162
+            f"{valor_str:<15}"                      # 163-177
+            f"{'':<40}"                             # 178-217
+            f"{'00':<2}"                            # 218-219
             f"{'':<10}"                             # 220-229
-            f"00"                                   # 230-231
-            f"045"                                  # 232-234: DDA
+            f"{'00':<2}"                            # 230-231
+            f"{'045':<3}"                           # 232-234
             f"{'':<6}"                              # 235-240
             f"\r\n"
         )
@@ -150,19 +156,22 @@ def gerar_cnab_pix(df_pagamentos):
         tipo_insc = "1" if len(doc_fav) <= 11 else "2"
 
         seg_b = (
-            f"13600013"                             # 01-08
+            f"{'136':<3}"                           # 01-03
+            f"{'0001':<4}"                          # 04-07
+            f"{'3':<1}"                             # 08-08
             f"{seq_lote:0>5}"                       # 09-13
-            f"B"                                    # 14-14
+            f"{'B':<1}"                             # 14-14
             f"{'':<3}"                              # 15-17
-            f"{tipo_insc}"                          # 18-18
-            f"{doc_fav:0>14}"                       # 19-32: CNPJ/CPF Chave
+            f"{tipo_insc:<1}"                       # 18-18
+            f"{doc_fav:0>14}"                       # 19-32
             f"{'':<30}"                             # 33-62
             f"{'0':<5}"                             # 63-67
             f"{'':<15}"                             # 68-82
             f"{'':<15}"                             # 83-97
             f"{'':<20}"                             # 98-117
-            f"{'00000000'}"                         # 118-125
-            f"SC"                                   # 126-127
+            f"{'00000':<5}"                         # 118-122
+            f"{'000':<3}"                           # 123-125
+            f"{'SC':<2}"                            # 126-127
             f"{'':<99}"                             # 128-226
             f"{'':<6}"                              # 227-232
             f"{'':<8}"                              # 233-240
@@ -178,10 +187,12 @@ def gerar_cnab_pix(df_pagamentos):
     valor_total_str = f"{int(total_valor * 100):0>18}"
     
     trailer_lote = (
-        f"13600015"
+        f"{'136':<3}"
+        f"{'0001':<4}"
+        f"{'5':<1}"
         f"{'':<9}"
         f"{qtd_lote_total:0>6}"
-        f"{valor_total_str}"
+        f"{valor_total_str:<18}"
         f"{'0':>18}"
         f"{'0':>6}"
         f"{'':<165}"
@@ -191,11 +202,13 @@ def gerar_cnab_pix(df_pagamentos):
 
     # --- TRAILER DE ARQUIVO (240 POSIÇÕES) ---
     trailer_arq = (
-        f"13699999"
+        f"{'136':<3}"
+        f"{'9999':<4}"
+        f"{'9':<1}"
         f"{'':<9}"
-        f"{1:0>6}"
+        f"{'000001':<6}"
         f"{qtd_lote_total+2:0>6}"
-        f"{'0':>6}"
+        f"{'000000':<6}"
         f"{'':<205}"
         f"\r\n"
     )
