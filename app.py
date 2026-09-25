a47616a1c256d940617074b6e7f5b9016e017578
Thiago Alves <153774707+thiagomkvl@users.noreply.github.com>
2026-09-25 11:37:55 -0300

    Fix typo in cockpit_itau page title

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ pg_pagar = st.Page("pages/painel_pagar.py", title="Painel de Pagamentos", icon="
 pg_obras = st.Page("pages/painel_obras.py", title="Acompanhamento de Obras", icon="🏗️")
 pg_cockpit = st.Page("pages/cockpit.py", title="Motor Cnab", icon="🏗️")
 pg_conciliação = st.Page("pages/conciliacao_recebiveis.py", title="Conciliação Mensal", icon="🏗️")
-pg_cockpititau = st.Page("pages/cockpit_itau.py", title="Cnab Itaí", icon="🏗️")
+pg_cockpititau = st.Page("pages/cockpit_itau.py", title="Cnab Itaú", icon="🏗️")
 
 
 
